6ce12ede22d116fd76e88857aae9f181cfdd1e43
ChristianTertius <ct160103@gmail.com>
2025-11-24 16:30:22 +0800

    (BE) Guest can GET/GET ALL, Auth user can POST/PUT/DELETE

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,12 +9,14 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 // public routes
 Route::post("/login", [AuthController::class, "login"])->name("login");
+
+
+Route::apiResource("bios", BioController::class)->only(["index", "show"]);;
+Route::apiResource("works", WorkController::class)->only(["index", "show"]);;
+Route::apiResource("projects", ProjectController::class)->only(["index", "show"]);;
+Route::apiResource("blogs", BlogController::class)->only(["index", "show"]);;
 
 // protected routes
 Route::middleware("auth:sanctum")->group(function () {
@@ -22,8 +24,9 @@ Route::middleware("auth:sanctum")->group(function () {
     Route::get("/me", [AuthController::class, "me"])->name("me");
 
     Route::apiResource("users", UserController::class);
-    Route::apiResource("bios", BioController::class);
-    Route::apiResource("works", WorkController::class);
-    Route::apiResource("projects", ProjectController::class);
-    Route::apiResource("blogs", BlogController::class);
+
+    Route::apiResource("bios", BioController::class)->except(["index", "show"]);;
+    Route::apiResource("works", WorkController::class)->except(["index", "show"]);;
+    Route::apiResource("projects", ProjectController::class)->except(["index", "show"]);;
+    Route::apiResource("blogs", BlogController::class)->except(["index", "show"]);;
 });
