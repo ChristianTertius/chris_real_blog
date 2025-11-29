46f0222036321f999d234d1b997897678f65f7bc
ChristianTertius <ct160103@gmail.com>
2025-11-29 22:50:34 +0800

    (FE) Footer Added

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <div className="p-5 text-center text-sm">© 2025 Christian Carlos Tertius | Created in the West Jakarta</div>
+  )
+}
+
+export default Footer;
